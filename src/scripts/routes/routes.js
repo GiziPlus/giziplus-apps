024803e3b444480eccf15b1b4b5fa6e8d27516c8
aldipeha@gmail.com <aldighifari0@gmail.com>
2023-06-08 19:49:23 +0700

    adding webpack

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,4 +1,4 @@
-import home from '../views/pages/home';
+import Home from '../views/pages/home';
 import test from '../views/pages/test';
 import article from '../views/pages/article';
 import forum from '../views/pages/forum';
@@ -6,13 +6,13 @@ import aboutUs from '../views/pages/aboutUs';
 import loginPage from '../views/pages/loginPage';
 
 const routes = {
-  '/': home, // default page
-  '/home': home,
+  '/': Home, // default page
+  '/home': Home,
   '/test': test,
   '/article/': article,
   '/forum/': forum,
   '/aboutUs/': aboutUs,
-  '/loginPage/': loginPage,
+  '/login/': loginPage,
 };
 
 export default routes;
