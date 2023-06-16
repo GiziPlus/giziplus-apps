7c9346095737ae4119b70e3caccda1bc039de86e
Muhammad Nur Ilmi <mnurilmi2016@gmail.com>
2023-06-16 21:37:47 +0700

    Adding Forum Api

--- src/scripts/global/api-endpoint.js
+++ src/scripts/global/api-endpoint.js
@@ -9,6 +9,44 @@ class ArticleSource {
   }
 }
 
+class ForumSource {
+  static async getForumList() {
+    const response = await fetch(`${CONFIG.FORUM_API_URL}`);
+    const responseJson = await response.json();
+
+    return responseJson.tours;
+  }
+
+  static async getForumDetail() {
+    const response = await fetch(`${CONFIG.FORUM_API_URL}:id`);
+    const responseJson = await response.json();
+
+    return responseJson.tours;
+  }
+
+  static async updateForumDetail() {
+    const response = await fetch(`${CONFIG.FORUM_API_URL}:id`);
+    const responseJson = await response.json();
+
+    return responseJson.tours;
+  }
+
+  static async createForumDetail() {
+    const response = await fetch(`${CONFIG.FORUM_API_URL}create`);
+    const responseJson = await response.json();
+
+    return responseJson.tours;
+  }
+
+  static async deleteForumDetail() {
+    const response = await fetch(`${CONFIG.FORUM_API_URL}:id`);
+    const responseJson = await response.json();
+
+    return responseJson.tours;
+  }
+}
+
 export {
   ArticleSource,
+  ForumSource,
 };
--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   NEWS_API_URL: 'https://api-berita-indonesia.vercel.app/suara/',
+  FORUM_API_URL: 'https://giziplus-apps-backend-production.up.railway.app/',
 };
 
 export default CONFIG;
